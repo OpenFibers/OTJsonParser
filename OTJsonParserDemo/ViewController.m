e0cd3931668b88341a5ea76d009399b188fc72cf
OpenThread <openfibers@gmail.com>
2016-01-25 15:59:59 +0800

    demo updated

--- OTJsonParserDemo/ViewController.m
+++ OTJsonParserDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "OTJsonParser.h"
 
 @interface ViewController ()
 
@@ -14,14 +15,15 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    NSDictionary *dictionary = @{@"key": @"value", @"key2": @"value2"};
+    NSString *jsonString = [dictionary JSONString];
+    NSDictionary *jsonObject = [jsonString objectFromJSONString];
+    
+    NSLog(@"\nJSONString: \n%@\n\nobjectFromJSONString:\n%@", jsonString, jsonObject);
 }
 
 @end
